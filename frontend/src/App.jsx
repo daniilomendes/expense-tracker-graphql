edda134613a3cd8b13dffa810b970de07986c6bb
Danilo Mendes <danilo.mirandha@gmail.com>
2024-04-27 15:30:52 -0300

    feat: ui design added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import TransactionPage from "./pages/TransactionPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import Header from "./components/ui/Header";
+
+function App() {
+  const authUser = true;
+
+  return (
+    <main>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/TransactionPage.jsx
@@ -0,0 +1,194 @@
+import { useState } from "react";
+import TransactionFormSkeleton from "../components/skeletons/TransactionFormSkeleton ";
+
+const TransactionPage = () => {
+  const [formData, setFormData] = useState({
+    description: "",
+    paymentType: "",
+    category: "",
+    amount: "",
+    location: "",
+    date: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("formData", formData);
+  };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  // if (loading) return <TransactionFormSkeleton />;
+
+  return (
+    <div className="h-screen max-w-4xl mx-auto flex flex-col items-center">
+      <p className="md:text-4xl text-2xl lg:text-4xl font-bold text-center relative z-50 mb-4 mr-4 bg-gradient-to-r from-pink-600 via-indigo-500 to-pink-400 inline-block text-transparent bg-clip-text">
+        Atualizar esta transação
+      </p>
+      <form
+        className="w-full max-w-lg flex flex-col gap-5 px-3 "
+        onSubmit={handleSubmit}
+      >
+        {/* TRANSACTION */}
+        <div className="flex flex-wrap">
+          <div className="w-full">
+            <label
+              className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+              htmlFor="description"
+            >
+              Transação
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="description"
+              name="description"
+              type="text"
+              placeholder="Aluguel, compras, salário, etc."
+              value={formData.description}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        {/* PAYMENT TYPE */}
+        <div className="flex flex-wrap gap-3">
+          <div className="w-full flex-1 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+              htmlFor="paymentType"
+            >
+              Tipo de pagamento
+            </label>
+            <div className="relative">
+              <select
+                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="paymentType"
+                name="paymentType"
+                onChange={handleInputChange}
+                defaultValue={formData.paymentType}
+              >
+                <option value={"card"}>Cartão</option>
+                <option value={"cash"}>Dinheiro</option>
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg
+                  className="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          {/* CATEGORY */}
+          <div className="w-full flex-1 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+              htmlFor="category"
+            >
+              Categoria
+            </label>
+            <div className="relative">
+              <select
+                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="category"
+                name="category"
+                onChange={handleInputChange}
+                defaultValue={formData.category}
+              >
+                <option value={"saving"}>Quardando</option>
+                <option value={"expense"}>Despesa</option>
+                <option value={"investment"}>Investimento</option>
+              </select>
+              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                <svg
+                  className="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          {/* AMOUNT */}
+          <div className="w-full flex-1 mb-6 md:mb-0">
+            <label
+              className="block uppercase text-white text-xs font-bold mb-2"
+              htmlFor="amount"
+            >
+              Quantia(R$)
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="amount"
+              name="amount"
+              type="number"
+              placeholder="150"
+              value={formData.amount}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+
+        {/* LOCATION */}
+        <div className="flex flex-wrap gap-3">
+          <div className="w-full flex-1 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+              htmlFor="location"
+            >
+              Local
+            </label>
+            <input
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="location"
+              name="location"
+              type="text"
+              placeholder="Goiás"
+              value={formData.location}
+              onChange={handleInputChange}
+            />
+          </div>
+
+          {/* DATE */}
+          <div className="w-full flex-1">
+            <label
+              className="block uppercase tracking-wide text-white text-xs font-bold mb-2"
+              htmlFor="date"
+            >
+              Data
+            </label>
+            <input
+              type="date"
+              name="date"
+              id="date"
+              className="appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-[11px] px-4 mb-3 leading-tight focus:outline-none
+						 focus:bg-white"
+              placeholder="Selecione a data"
+              value={formData.date}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+
+        {/* SUBMIT BUTTON */}
+        <button
+          className="text-white font-bold w-full rounded px-4 py-2 bg-gradient-to-br
+          from-pink-500 to-pink-500 hover:from-pink-600 hover:to-pink-600"
+          type="submit"
+        >
+          Atualizar transação
+        </button>
+      </form>
+    </div>
+  );
+};
+export default TransactionPage;
